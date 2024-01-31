bfa7618013e8731424e7cded084ef76bfff2b6c7
Всеволод <kreepmeister@gmail.com>
2024-01-31 19:29:35 +0300

    Fix profile link

--- app/utils/messages.py
+++ app/utils/messages.py
@@ -2,7 +2,7 @@ def commit_message(res):
     modified = "\n".join([file for file in res["head_commit"]["modified"]])
     return f"""<b>📏 {res["repository"]["full_name"]} new commit!</b>
 <i>{res["head_commit"]["message"]}</i>
-<a href="{res["compare"]}">#{res["head_commit"]["id"][:7]}</a> by <i>{res["head_commit"]["author"]["name"]} (@{res["head_commit"]["author"]["username"]})</i>
+<a href="{res["compare"]}">#{res["head_commit"]["id"][:7]}</a> by <i>{res["head_commit"]["author"]["name"]} (<a href="{res["sender"]["html_url"]}">@{res["head_commit"]["author"]["username"]}</a>)</i>
 
 <b>🖊 Modified files:</b>
 <code>{modified}</code>
